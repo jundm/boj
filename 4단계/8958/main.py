0536fb55c9ef95f28ee8f12bd06a70794c34fdf5
jundm <bnmva23@gmail.com>
2022-05-16 00:16:32 +0900

    feat:solved

--- 4단계/8958/main.py
+++ 4단계/8958/main.py
@@ -1,4 +1,14 @@
 import sys
 
-input = sys.stdin.read().splitlines()
-print()
+input = sys.stdin.read().splitlines()[1:]
+
+for j in range(len(input)):
+    X = 0
+    result = 0
+    for i in input[j]:
+        if i == "O":
+            X += 1
+        else:
+            X = 0
+        result += X
+    print(result)
